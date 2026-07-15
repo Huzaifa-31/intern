84277c0bc63cdfba33829821a604a072bc0c1850
adeelfaisal701 <adeelfaisal701@gmail.com>
2026-07-15 16:47:58 +0500

    1-auth-sys

---
+++ src/app/adeel/page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1 className="text-center text-5xl">digi ditos is a software company</h1>
+  );
+}
---
+++ src/app/faisal/page.js
@@ -0,0 +1,5 @@
+export default function Digivlogs() {
+  return (
+    <h1 className="text-center"><p>Hi everyone</p></h1>
+  );
+}
